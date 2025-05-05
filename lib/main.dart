d0be948ba34a1db56e819f3ca60e4b217fc03aae
powdevlin12 <thudatdl123@gmail.com>
2025-05-05 22:23:58 +0700

    setup firebase ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:dat_chat/colors.dart';
 import 'package:dat_chat/features/landing/screens/landing_screen.dart';
 import 'package:dat_chat/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
